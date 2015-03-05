27dad10a18c0e8f0e9f825cc73939cb6e30a91b8
Stephan Göppentin <dedda102@gmail.com>
2015-03-05 11:39:50 +0100

    removed ids from data filling script

--- sql/data.sql
+++ sql/data.sql
@@ -1,53 +1,53 @@
 -- Guests
-INSERT INTO guest(id, name, surname)
-VALUES(1, 'Hans', 'Zimmer');
+INSERT INTO guest(name, surname)
+VALUES('Hans', 'Zimmer');
 
-INSERT INTO guest(id, name, surname)
-VALUES(2, 'Horst', 'Klein');
+INSERT INTO guest(name, surname)
+VALUES('Horst', 'Klein');
 
 -- Room Categories
-INSERT INTO room_category(id, name, price)
-VALUES(1, 'Basic', 500);
+INSERT INTO room_category(name, price)
+VALUES('Basic', 500);
 
-INSERT INTO room_category(id, name, price)
-VALUES(2, 'Luxus', 2000);
+INSERT INTO room_category(name, price)
+VALUES('Luxus', 2000);
 
-INSERT INTO room_category(id, name, price)
-VALUES(3, 'Giga', 10000);
+INSERT INTO room_category(name, price)
+VALUES('Giga', 10000);
 
 -- Rooms
-INSERT INTO room(id, people, category)
-VALUES(1, 2, 1);
+INSERT INTO room(people, category)
+VALUES(2, 1);
 
-INSERT INTO room(id, people, category)
-VALUES(2, 300, 3);
+INSERT INTO room(people, category)
+VALUES(300, 3);
 
-INSERT INTO room(id, people, category)
-VALUES(3, 5, 2);
+INSERT INTO room(people, category)
+VALUES(5, 2);
 
 --Staff Categories
-INSERT INTO staff_category(id, salary, name)
-VALUES(1, 20000, 'Bozz');
+INSERT INTO staff_category(salary, name)
+VALUES(20000, 'Bozz');
 
-INSERT INTO staff_category(id, salary, name)
-VALUES(2, 50, 'Putze');
+INSERT INTO staff_category(salary, name)
+VALUES(50, 'Putze');
 
 -- Staff
-INSERT INTO staff(id, name, surname, category, recruitement)
-VALUES(1, 'Herbert', 'Herbertsen', 1, '1961-06-16');
+INSERT INTO staff(name, surname, category, recruitement)
+VALUES('Herbert', 'Herbertsen', 1, '1961-06-16');
 
-INSERT INTO staff(id, name, surname, category, recruitement, firing)
-VALUES(2, 'Hassan', 'Assi', 2, '1990-01-01', '2010-01-01');
+INSERT INTO staff(name, surname, category, recruitement, firing)
+VALUES('Hassan', 'Assi', 2, '1990-01-01', '2010-01-01');
 
-INSERT INTO staff(id, name, surname, category, recruitement)
-VALUES(3, 'Mustafa', 'Kanacke', 2, '1990-01-01');
+INSERT INTO staff(name, surname, category, recruitement)
+VALUES('Mustafa', 'Kanacke', 2, '1990-01-01');
 
 -- Optional Services
-INSERT INTO optional_service(id, name, price)
-VALUES(1, 'Bootstour', 45);
+INSERT INTO optional_service(name, price)
+VALUES('Bootstour', 45);
 
-INSERT INTO optional_service(id, name, price)
-VALUES(2, 'Massage', 20);
+INSERT INTO optional_service(name, price)
+VALUES('Massage', 20);
 
 -- Staff to Room
 
@@ -70,11 +70,11 @@ VALUES(3, 2);
 
 -- Reservations
 
-INSERT INTO reservation(id, additional_info, reservation_group, room, guest, people, start_date, end_date)
-VALUES(1, '', 1, 2, 2, 12, '2015-01-01', '2015-12-31');
+INSERT INTO reservation(additional_info, reservation_group, room, guest, people, start_date, end_date)
+VALUES('', 1, 2, 2, 12, '2015-01-01', '2015-12-31');
 
-INSERT INTO reservation(id, additional_info, reservation_group, room, guest, people, start_date, end_date)
-VALUES(2, '', 2, 1, 1, 3, '2015-03-02', '2015-06-01');
+INSERT INTO reservation(additional_info, reservation_group, room, guest, people, start_date, end_date)
+VALUES('', 2, 1, 1, 3, '2015-03-02', '2015-06-01');
 
 -- Services to Reservations
 
