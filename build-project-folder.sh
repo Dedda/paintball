073ddb4121469d648f9205b6901f9faff3174ab0
Stephan Goeppentin <stephan.goeppentin@adition.com>
2015-06-11 17:04:42 +0200

    small style changes

--- build-project-folder.sh
+++ build-project-folder.sh
@@ -16,6 +16,7 @@ done
 PROJECT_FOLDER=$1
 
 if [ -d $PROJECT_FOLDER ]; then
+    echo "removing existing project folder"
     rm -rf $PROJECT_FOLDER
 fi
 
@@ -25,7 +26,6 @@ fi
 
 mkdir "$PROJECT_FOLDER"
 mkdir "$PROJECT_FOLDER/distribution"
-mkdir "$PROJECT_FOLDER/distribution/lib"
 mkdir "$PROJECT_FOLDER/source"
 echo "copying project folder to $PROJECT_FOLDER/source..."
 cp -r ./* "$PROJECT_FOLDER/source/"
@@ -35,11 +35,13 @@ ant jar javadoc > /dev/null
 cp "./dist/paintball.jar" "$PROJECT_FOLDER/distribution/"
 
 echo "copying libs and sql scripts..."
-cp -r       "./lib"             "$PROJECT_FOLDER/distribution/"
-cp          ./sql/* "$PROJECT_FOLDER/"
+cp -r       "./lib"                     "$PROJECT_FOLDER/distribution/"
+cp          ./sql/*                     "$PROJECT_FOLDER/"
 chmod +x    "$PROJECT_FOLDER/install.sh"
-cp -r       "./documentation"   "$PROJECT_FOLDER/dok"
-cp -r       "./dist/javadoc"    "$PROJECT_FOLDER/dok/"
+cp -r       "./documentation"           "$PROJECT_FOLDER/dok"
+cp          ./documentation/index.html  "$PROJECT_FOLDER/dok/"
+cp -r       "./dist/javadoc"            "$PROJECT_FOLDER/dok/"
+cp          "./README.md"               "$PROJECT_FOLDER/dok/"
 
 echo "converting odt files to pdf... make sure all instances of libre office are closed!"
 cd "$PROJECT_FOLDER/dok"
@@ -60,7 +62,6 @@ fi
 
 
 cd $PROJECT_FOLDER
-echo -n "project directory created at "
-pwd
+echo "project directory created at $(pwd)"
 
 cd - > /dev/null
