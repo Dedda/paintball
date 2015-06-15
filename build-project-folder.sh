acee961557cb320bbaeb08aba332590ebdf80abf
Stephan Goeppentin <stephan.goeppentin@adition.com>
2015-06-15 15:46:16 +0200

    added uml diagrams

--- build-project-folder.sh
+++ build-project-folder.sh
@@ -30,15 +30,15 @@ if [ -d $PROJECT_FOLDER ]; then
     rm -rf $PROJECT_FOLDER
 fi
 
-if [ -d "./dist/" ]; then
-    rm -rf "./dist/"
+if [ -d ./dist/ ]; then
+    rm -rf ./dist/
 fi
 
-mkdir "$PROJECT_FOLDER"
-mkdir "$PROJECT_FOLDER/distribution"
-mkdir "$PROJECT_FOLDER/source"
+mkdir $PROJECT_FOLDER
+mkdir $PROJECT_FOLDER/distribution
+mkdir $PROJECT_FOLDER/source
 echo "copying project folder to $PROJECT_FOLDER/source..."
-cp -r ./* "$PROJECT_FOLDER/source/"
+cp -r ./* $PROJECT_FOLDER/source/
 
 cd $PROJECT_FOLDER
 ABSOLUTE_PROJECT_PATH=$(pwd)
@@ -46,7 +46,7 @@ cd - > /dev/null
 
 echo "compiling and generating javadoc..."
 ant jar javadoc > /dev/null
-cp "./dist/paintball.jar" "$PROJECT_FOLDER/distribution/"
+cp ./dist/paintball.jar $PROJECT_FOLDER/distribution/
 echo -e "#!/usr/bin/env bash\njava -jar $ABSOLUTE_PROJECT_PATH/distribution/paintball.jar" > $PROJECT_FOLDER/distribution/paintball.sh
 chmod +x $PROJECT_FOLDER/distribution/paintball.sh
 
@@ -55,24 +55,27 @@ cp -r       "./lib"                     "$PROJECT_FOLDER/distribution/"
 cp          ./sql/*                     "$PROJECT_FOLDER/"
 chmod +x    "$PROJECT_FOLDER/install.sh"
 cp -r       "./documentation"           "$PROJECT_FOLDER/dok"
+rm -f       $PROJECT_FOLDER/dok/erm/*.png &> /dev/null
+echo "generating deiagrams from puml"
 java -jar   $PROJECT_FOLDER/distribution/lib/plantuml.jar "$PROJECT_FOLDER/dok/erm/"
 cp          ./documentation/index.html  "$PROJECT_FOLDER/dok/"
 cp -r       "./dist/javadoc"            "$PROJECT_FOLDER/dok/"
 cp          "./README.md"               "$PROJECT_FOLDER/dok/"
 
 echo "converting odt files to pdf... make sure all instances of libre office are closed!"
-cd "$PROJECT_FOLDER/dok"
+cd $PROJECT_FOLDER/dok
 libreoffice --headless --convert-to pdf *.odt > /dev/null
 cd - > /dev/null
 
 
 echo "cleaning up..."
-rm -rf "./dist/"
-rm "$PROJECT_FOLDER/source/build-project-folder.sh"
+rm -rf ./dist/
+rm $PROJECT_FOLDER/source/build-project-folder.sh
+rm -rf $PROJECT_FOLDER/source/documentation/
 
 if [ $INSTALL == 1 ]; then
     echo "installation triggered"
-    cd "$PROJECT_FOLDER"
+    cd $PROJECT_FOLDER
     ./install.sh
     cd - > /dev/null
 fi
@@ -80,7 +83,7 @@ fi
 if [ $DESKTOP_LINK == 1 ]; then
     echo "create link on desktop"
     cd ~/Desktop
-    ln -fs "$ABSOLUTE_PROJECT_PATH/distribution/paintball.sh" "./$DESKTOP_LINK_NAME"
+    ln -fs $ABSOLUTE_PROJECT_PATH/distribution/paintball.sh ./$DESKTOP_LINK_NAME
 fi
 
 echo "project directory created at $ABSOLUTE_PROJECT_PATH"
